c680549d52b41f696a04a4dd55bfd7c42e7780f8
LuisLlumiquinga <luisllumiquinga.clearminds@gmail.com>
2026-07-04 09:02:00 -0500

    fix: vercel config

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { createApp } from '../src/app.js'
-import serverless from 'serverless-http'
 
 const app = createApp()
-export default serverless(app)
+export default app
